2c19cadbc11d5208215dbc3d0df0a99ee91d2106
Björn Folbert <b@folbert.com>
2016-02-09 10:41:43 +0100

    Fix for activating field snitch
    Scripts and styles were enqueued to soon. That has been fixed now.

--- extras/acf-field-snitch/activate.php
+++ extras/acf-field-snitch/activate.php
@@ -1,7 +1,12 @@
 <?php
 
-wp_enqueue_script( 'fewbricks-acf-snitch.js', get_template_directory_uri() . '/fewbricks/extras/acf-field-snitch/snitch.js', ['jquery'], '1.0.0' );
+namespace fewbricks\acf_field_snitch;
 
-//wp_enqueue_style( 'fewture-admin-css', get_template_directory_uri() . '/fewture/backend/assets/css/admin.css', [], '1.0.0' );
-wp_enqueue_style( 'fewture-project-css', get_template_directory_uri() . '/fewbricks/extras/acf-field-snitch/snitch.css', [], '1.0.0' );
+add_action('admin_enqueue_scripts' , __NAMESPACE__ . '\\scripts_and_styles');
 
+function scripts_and_styles() {
+
+    wp_enqueue_script( 'fewbricks-acf-snitch.js', get_template_directory_uri() . '/fewbricks/extras/acf-field-snitch/snitch.js', ['jquery'], '1.0.0' );
+    wp_enqueue_style( 'fewture-project-css', get_template_directory_uri() . '/fewbricks/extras/acf-field-snitch/snitch.css', [], '1.0.0' );
+
+}
