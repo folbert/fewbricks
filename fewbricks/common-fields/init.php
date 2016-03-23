09fd04a06c84156f83c338142333e0544f4950be
folbert <b@folbert.com>
2016-03-23 20:43:34 +0100

    Added warnings about not deleting vital files

--- fewbricks/common-fields/init.php
+++ fewbricks/common-fields/init.php
@@ -1,3 +1,7 @@
 <?php
 
+/**
+ * Do not delete this file! But if you will not be using the common fields functionality, feel free to delete all content in it.
+ */
+
 require('common-fields-demo.php');
--- fewbricks/field-groups/init.php
+++ fewbricks/field-groups/init.php
@@ -1,6 +1,10 @@
 <?php
 
 /**
+ * Do not delete this file!
+ */
+
+/**
  * Use this file to require all other field group.
  * DO NOT use require_once since that will break stuff.
  */
