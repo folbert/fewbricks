db2793a746ac8681472d5d47debe0be61526cbfe
folbert <b@folbert.com>
2016-03-25 23:07:05 +0100

    Added describing comments to required files

--- fewbricks/common-fields/init.php
+++ fewbricks/common-fields/init.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * Do not delete this file! But if you will not be using the common fields functionality, feel free to delete all content in it.
+ * Do not delete this file!
+ * But if you will not be using the common fields functionality, feel free to delete all content in it.
  */
 
 require('common-fields-demo.php');
