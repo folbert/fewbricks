abe76ebb4e66b447ffa8691e888b552f73ae4638
folbert <b@folbert.com>
2016-03-28 20:52:52 +0200

    Updated PhpDoc block for all ACF field classes

--- lib/acf/fields/field.php
+++ lib/acf/fields/field.php
@@ -20,11 +20,14 @@ class field
     private $key_prepared;
 
     /**
-     * @param string $label
-     * @param string $name
-     * @param string $key
+     * @param string $label The label that the field will get.
+     * @param string $name The name that the field will get.
+     * @param string $key The key that the field wil get. It is very important that this value is unique among
+     * all the keys across the entire site.
      * @param array $base_settings
-     * @param array $custom_settings
+     * @param array $custom_settings Any custom settings that you want to set. A setting must be implemented in the ACF
+     * field class for it to have an effect. This array wil be merged with $base_settings in this class and then with
+     * the default settings in the ACF field class.
      */
     public function __construct($label, $name, $key, $base_settings = [], $custom_settings = [])
     {
--- lib/acf/fields/true-false.php
+++ lib/acf/fields/true-false.php
@@ -10,10 +10,13 @@ class true_false extends field
 {
 
     /**
-     * @param string $label
-     * @param string $name
-     * @param string $key
-     * @param array $custom_settings
+     * @param string $label The label that the field will get.
+     * @param string $name The name that the field will get.
+     * @param string $key The key that the field wil get. It is very important that this value is unique among
+     * all the keys across the entire site.
+     * @param array $custom_settings Any custom settings that you want to set. A setting must be implemented in the ACF
+     * field class for it to have an effect. This array wil be merged with $base_settings in this class and then with
+     * the default settings in the ACF field class.
      */
     public function __construct($label, $name, $key, $custom_settings = [])
     {
