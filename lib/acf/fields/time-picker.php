7cfa4c5b9083484a1f14e38730aa2bb77cd3053a
Marie Manandise <marie@studio24.net>
2017-11-30 16:23:46 +0000

    added support for time picker field

---
+++ lib/acf/fields/time-picker.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace fewbricks\acf\fields;
+
+/**
+ * Class date_picker
+ * @package fewbricks\acf\fields
+ */
+class time_picker extends field
+{
+
+    /**
+     * @param string $label The label that the field will get.
+     * @param string $name The name that the field will get.
+     * @param string $key The key that the field wil get. It is very important that this value is unique among
+     * all the keys across the entire site.
+     * @param array $custom_settings Any custom settings that you want to set. A setting must be implemented in the ACF
+     * field class for it to have an effect. This array wil be merged with $base_settings in this class and then with
+     * the default settings in the ACF field class.
+     */
+    public function __construct($label, $name, $key, $custom_settings = [])
+    {
+
+        $base_settings = [
+            'prefix' => '',
+            'type' => 'time_picker',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => [
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ],
+            'display_format' => 'H:i:s',
+            'return_format' => 'H:i:s'
+        ];
+
+        parent::__construct($label, $name, $key, $base_settings, $custom_settings);
+
+    }
+
+}
