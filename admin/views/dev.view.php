11e5f67b028f5e9ac143c2b59689cd603cb9883a
Björn Folbert <bjorn.folbert@kan.se>
2017-12-12 15:54:58 +0100

    Began work on admin pages [WIP]

--- admin/views/dev.view.php
+++ admin/views/dev.view.php
@@ -1,4 +1,22 @@
 <div class="wrap">
     <h1>Fewbricks</h1>
 
+
+    <div class="acf-meta-box-wrap -grid">
+        <div class="postbox">
+
+            <h2 class="hndle">Export to PHP</h2>
+
+            <div class="inside">
+
+                <p>Here you can export all the field groups and fields that are registered using Fewbricks</p>
+                <form action="" method="post">
+                    <?php submit_button('Export to PHP', 'primary', 'fewbricks_export_to_php'); ?>
+                </form>
+            </div>
+
+        </div>
+
+    </div>
+
 </div>
